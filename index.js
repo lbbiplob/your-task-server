19120aa588cd0ab96c3a8613202806436b159143
lbbiplob <biplab.jsnu@gmail.com>
2022-12-29 19:58:06 +0600

    task post and task get ok

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.send("server is running");
@@ -21,14 +22,22 @@ const client = new MongoClient(uri, {
 const run = async () => {
   try {
     const taskCollection = client.db("yourTask").collection("task");
+
+    app.post("/task", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+
+    app.get("/task", async (req, res) => {
+      const query = {};
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
   } finally {
   }
 };
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 run().catch((err) => console.log(err));
 app.listen(port, () => {
   console.log(`This app is running in ${port}`);
