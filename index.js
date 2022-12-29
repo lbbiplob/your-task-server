ae025365f2216d86c28f95dedf08ce3a24e6b900
lbbiplob <biplab.jsnu@gmail.com>
2022-12-29 23:15:23 +0600

    delete added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -32,6 +32,14 @@ const run = async () => {
     app.get("/task", async (req, res) => {
       const query = {};
       const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: ObjectId(id) };
+      const result = await taskCollection.deleteOne(filter);
       res.send(result);
     });
   } finally {
